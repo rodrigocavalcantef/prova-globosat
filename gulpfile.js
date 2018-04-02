211fa45ae52cb0156a905ce31c7daf0dc37dae60
Rodrigo Cavalcante <rodrigocferreira87@gmail.com>
2018-04-02 07:23:06 -0300

    Subindo teste.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp')
+  ,imagemin = require('gulp-imagemin');
+
+gulp.task('build-img', function() {
+
+  gulp.src('img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('img'));
+});
